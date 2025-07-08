006b6350714668c61c54e600c3c8f1f2ec5eee1d
iFinn <121723759+ifinnapp@users.noreply.github.com>
2025-07-08 10:46:52 +0700

    Create paymentSuccess.php

---
+++ pay2finn/public/paymentSuccess.php
@@ -0,0 +1,16 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<div class="container">
+	<div class="row text-center">
+        <div class="col-sm-6 col-sm-offset-3">
+        <br><br> <h2 style="color:#0fad00">Success</h2>
+        <img src="https://t3.ftcdn.net/jpg/03/14/30/00/360_F_314300005_uZOXHOY3rLnfGISg47qOQKBFyb8ZXb0I.jpg">
+        <p style="font-size:20px;color:#5C5C5C;">ทางเราได้รับยอดการชำระเงินของท่านเรียบร้อยแล้ว</p>
+    <br><br>
+        </div>
+        
+	</div>
+</div>
