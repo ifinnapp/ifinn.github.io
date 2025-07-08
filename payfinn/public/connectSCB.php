11f62d85d1f623b5da8faeae7b9ba43f64bb6b25
iFinn <121723759+ifinnapp@users.noreply.github.com>
2025-07-08 10:59:02 +0700

    Create connectSCB.php

---
+++ payfinn/public/connectSCB.php
@@ -0,0 +1,103 @@
+<?php
+    function scbAuth(){
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://api-sandbox.partners.scb/partners/sandbox/v1/oauth/token',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS =>'{
+            "applicationKey" : "l793a52b5830274acb892356ea49b2740c",
+            "applicationSecret" : "01f7ddd3bf6d457d9e5815a2619b8ef9"
+        }',
+        CURLOPT_HTTPHEADER => array(
+            'Content-Type: application/json',
+            'resourceOwnerId: l793a52b5830274acb892356ea49b2740c',
+            'requestUId: dd6b995f-9fe3-492e-b57c-4d44b6191ba1',
+            'accept-language: EN',
+            'Cookie: TS01e7ba6b=01e76b033cfb6831b291c19a858709272ffe70aeb758e86cc52cd741cd2d8a06045b676a7c8b2423eb09b8e56af9bb691fea30e71d'
+        ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        return json_decode($response,true);
+    }
+
+    function genSCBDeeplink($amount,$refCode,$scbAccessToken)
+    {
+        $datas = [];
+        $datas['transactionType'] = "PURCHASE";
+        $datas['transactionSubType'] = ["BP", "CCFA", "CCIPP"];
+        $datas['sessionValidityPeriod'] = 60;
+        $datas['sessionValidUntil'] = "";
+
+        $datas['billPayment']['paymentAmount'] = $amount;
+        $datas['billPayment']['accountTo'] = "015768847876768";
+        $datas['billPayment']['ref1'] = $refCode;
+        $datas['billPayment']['ref2'] = "01";
+        $datas['billPayment']['ref3'] = "SCB";
+
+        $datas['creditCardFullAmount']['merchantId'] = "430459283460796";
+        $datas['creditCardFullAmount']['terminalId'] = "316577479545726";
+        $datas['creditCardFullAmount']['orderReference'] = $refCode;
+        $datas['creditCardFullAmount']['paymentAmount'] = $amount;
+
+        $datas['installmentPaymentPlan']['merchantId'] = "430459283460796";
+        $datas['installmentPaymentPlan']['terminalId'] = "316577479545726";
+        $datas['installmentPaymentPlan']['orderReference'] = $refCode;
+        $datas['installmentPaymentPlan']['paymentAmount'] = $amount;
+        $datas['installmentPaymentPlan']['tenor'] = "12";
+        $datas['installmentPaymentPlan']['ippType'] = "3";
+        $datas['installmentPaymentPlan']['prodCode'] = "1001";
+
+        $datas['merchantMetaData']['callbackUrl'] = "https://payfinn.glitch.me/public/success.php";
+        $datas['merchantMetaData']['merchantInfo']['name'] = "TEST";
+        $datas['merchantMetaData']['extraData'] = "";
+        $datas['merchantMetaData']['paymentInfo'][0]['type'] = "TEXT_WITH_IMAGE";
+        $datas['merchantMetaData']['paymentInfo'][0]['title'] = "";
+        $datas['merchantMetaData']['paymentInfo'][0]['header'] = "";
+        $datas['merchantMetaData']['paymentInfo'][0]['description'] = "";
+        $datas['merchantMetaData']['paymentInfo'][0]['imageUrl'] = "";
+
+        $datas['merchantMetaData']['paymentInfo'][1]['type'] = "TEXT_WITH_IMAGE";
+        $datas['merchantMetaData']['paymentInfo'][1]['title'] = "";
+        $datas['merchantMetaData']['paymentInfo'][1]['header'] = "";
+        $datas['merchantMetaData']['paymentInfo'][1]['description'] = "";
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://api-sandbox.partners.scb/partners/sandbox/v3/deeplink/transactions',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => json_encode($datas),
+        CURLOPT_HTTPHEADER => array(
+            'Content-Type: application/json',
+            'authorization: Bearer '.$scbAccessToken,
+            'resourceOwnerId: l793a52b5830274acb892356ea49b2740c',
+            'requestUId: dd6b995f-9fe3-492e-b57c-4d44b6191ba1',
+            'channel: scbeasy',
+            'accept-language: EN',
+            'Cookie: TS01e7ba6b=01a990b48e9743982bf33136414fecc4291ebe2dbca4c6a78368e1b35460a233bfa3dbb3853d79a41fbd33c38b84a95fc3ef577cb3'
+        ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        return json_decode($response,true);
+    }
